ea8f17e68c0c05f10c49479803ee740bc932513b
ChoiWheatley <chltmdgus604@naver.com>
2023-04-01 01:43:02 +0900

    [tmll][ch02] iter impl with lots of lifetime

--- too_many_linked_lists/src/ch02_ok_linked_stack.rs
+++ too_many_linked_lists/src/ch02_ok_linked_stack.rs
@@ -66,6 +66,37 @@ impl<T> Drop for List<T> {
     }
 }
 
+/// IntoIter **consumes** data members
+pub struct IntoIter<T>(List<T>);
+/// Iter only borrows `&Node<T>` but we must ensure lifetime is valid during being used!
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+/// IterMut borrows with mutation
+pub struct IterMut<T>(List<T>);
+
+impl<T> List<T> {
+    /// List contains head, which is type of `Option<Box<Node<T>>>` pew...
+    ///
+    /// 1. desired result: `Option<&'a Node<T>>`
+    /// 2. head.as_ref() becomes `Option<&Box<Node<T>>>`
+    /// 3. convert option type with map => node will be `&Box<Node<T>>` if node is Some
+    /// 4. node.as_ref() becomes `&Node<T>`
+    /// 5. return of map becomes `Option<&Node<T>>`
+    ///
+    /// However, `as_deref()` automatically dereferences inner type
+    /// (from `Option<T>` to `Option<&T>`) so we don't need to do such thing!!! SICK
+    ///
+    /// Let's move on to the impl of [Iter<'a, T>]
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            next: self.head.as_deref(), // same as below
+                                        // next: self.head.as_ref().map(|node| node.as_ref()),  // same as below
+                                        // next: self.head.as_ref().map(|boxed_node| &**boxed_node),
+        }
+    }
+}
+
 /// IntoIterator only exposes method `into_iter`.
 /// Concrete implementations are in custom iterator `IntoIter<T>`
 impl<T> IntoIterator for List<T> {
@@ -78,9 +109,6 @@ impl<T> IntoIterator for List<T> {
     }
 }
 
-/// IntoIter **consumes** data members
-pub struct IntoIter<T>(List<T>);
-
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
 
@@ -90,8 +118,22 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    /// Finally, Iter gives us reference of elements!
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref(); // same as below
+                                              // self.next = node.next.as_ref().map::<&Node<T>, _>(|node| &node);
+            &node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
+
     use super::*;
     #[test]
     fn basics() {
@@ -160,5 +202,36 @@ mod tests {
         assert_eq!(Some(1), iter.next());
 
         assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push("hi")
+            .push("my")
+            .push("name")
+            .push("is")
+            .push("choi")
+            .push("wheatley");
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&"wheatley"));
+        assert_eq!(iter.next(), Some(&"choi"));
+        assert_eq!(iter.next(), Some(&"is"));
+        assert_eq!(iter.next(), Some(&"name"));
+        assert_eq!(iter.next(), Some(&"my"));
+        assert_eq!(iter.next(), Some(&"hi"));
+
+        // iter dosen't consume anything!!!
+        assert_eq!(list.peek(), Some(&"wheatley"));
+    }
+
+    #[test]
+    fn as_deref() {
+        struct Node(i32);
+        let original = Node(1);
+        let option_box_node: Option<Box<Node>> = Some(Box::new(original));
+        let _option_borrowed: Option<&Node> = option_box_node.as_ref().map(|e| &**e); // same as below
+        let _option_borrowed: Option<&Node> = option_box_node.as_ref().map::<&Node, _>(|e| e); // same as below
+        let _option_borrowed: Option<&Node> = option_box_node.as_deref();
     }
 }
