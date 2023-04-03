080f89b5e7b4a65e562e3582f59be136196c37d1
ChoiWheatley <chltmdgus604@naver.com>
2023-04-04 01:02:24 +0900

    [tmll][ch04] bad doubly linked deque

---
+++ too_many_linked_lists/src/ch04_bad_doubly_linked_deque.rs
@@ -0,0 +1,109 @@
+#![allow(unused)]
+// be aware of accidently using borrow::BorrowMut!
+// It is implemented via "blanket implementations" both
+// `Rc` and `RefCell`, which can cause infinite `borrow_mut()` loop!
+// use std::borrow::BorrowMut;
+use std::cell::RefCell;
+use std::rc::Rc;
+/// Let's make doubly-linked deque!!!
+///
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                // non-empty list, need to connect the old_head
+                old_head.borrow_mut().prev = Some(new_head.clone());
+                new_head.borrow_mut().next = Some(old_head);
+                self.head = Some(new_head);
+            }
+            None => {
+                // empty list, need to set the tail
+                self.tail = Some(new_head.clone());
+                self.head = Some(new_head);
+            }
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    // not emptying list
+                    new_head.borrow_mut().prev.take();
+                    self.head = Some(new_head); // replace head to a new one
+                }
+                None => {
+                    // emptying list
+                    self.tail.take();
+                }
+            }
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem,
+            next: None,
+            prev: None,
+        }))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_front(), None);
+
+        // Populate list
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_front(4);
+        list.push_front(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+    }
+}
--- too_many_linked_lists/src/main.rs
+++ too_many_linked_lists/src/main.rs
@@ -1,6 +1,7 @@
 pub mod ch01_bad_stack;
 pub mod ch02_ok_linked_stack;
 mod ch03_persistent_stack;
+mod ch04_bad_doubly_linked_deque;
 fn main() {
     println!("Hello, world!");
 }
