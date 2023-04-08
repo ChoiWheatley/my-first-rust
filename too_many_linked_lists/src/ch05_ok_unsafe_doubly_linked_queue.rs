bd8db198e4fce0ae4c67f1df97dd15c33dda725e
ChoiWheatley <chltmdgus604@naver.com>
2023-04-08 16:39:22 +0900

    [tmll][ch05] iterators

--- too_many_linked_lists/src/ch05_ok_unsafe_doubly_linked_queue.rs
+++ too_many_linked_lists/src/ch05_ok_unsafe_doubly_linked_queue.rs
@@ -20,6 +20,14 @@ struct Node<T> {
     next: Link<T>,
 }
 
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+pub struct IntoIter<T>(List<T>);
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List {
@@ -51,7 +59,7 @@ impl<T> List<T> {
             None
         } else {
             unsafe {
-                // Re-wrap into Box
+                // Re-wrap into Box, this object will be dropped when this block ends
                 let ret = Box::from_raw(self.head);
 
                 self.head = ret.next;
@@ -65,11 +73,69 @@ impl<T> List<T> {
             }
         }
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        unsafe { self.head.as_ref().map(|node| &node.elem) }
+    }
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        unsafe { self.head.as_mut().map(|node| &mut node.elem) }
+    }
 }
 
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
         while let Some(cur) = self.pop() {}
+    }
+}
+
+impl<'a, T> List<T> {
+    /// consumes list and iterate elements
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            next: unsafe { self.head.as_ref() },
+        }
+    }
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: unsafe { self.head.as_mut() },
+        }
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.take().map(|node| {
+                self.next = node.next.as_ref();
+                &node.elem
+            })
+        }
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.take().map(|node| {
+                self.next = node.next.as_mut();
+                &mut node.elem
+            })
+        }
     }
 }
 
@@ -86,5 +152,82 @@ mod test {
 
         ls.iter().for_each(|e| assert_eq!(Some(e), list.pop()));
         assert_eq!(None, list.pop());
+    }
+
+    #[test]
+    fn peek() {
+        let ls = [0, 1, 2, 3, 4, 5];
+        let mut list = List::new();
+
+        ls.iter().cloned().for_each(|e| list.push(e));
+
+        assert_eq!(Some(&0), list.peek());
+        assert_eq!(
+            Some(&mut 100),
+            list.peek_mut().map(|elem| {
+                *elem += 100;
+                elem
+            })
+        );
+    }
+
+    #[test]
+    fn into_iter() {
+        let ls = [0, 1, 2, 3, 4, 5];
+        let mut list = List::new();
+
+        ls.iter().cloned().for_each(|e| list.push(e));
+
+        let mut consumed_iterator = list.into_iter();
+        let mut answer_iterator = ls.iter();
+
+        while let Some(answer) = answer_iterator.next() {
+            assert_eq!(*answer, consumed_iterator.next().unwrap());
+        }
+
+        assert_eq!(None, consumed_iterator.next());
+    }
+
+    #[test]
+    fn iter() {
+        let ls = [0, 1, 2, 3, 4, 5];
+        let mut list = List::new();
+
+        ls.iter().cloned().for_each(|e| list.push(e));
+
+        let mut iter = list.iter();
+        assert_eq!(Some(&0), iter.next());
+        assert_eq!(Some(&1), iter.next());
+        assert_eq!(Some(&2), iter.next());
+        assert_eq!(Some(&3), iter.next());
+        assert_eq!(Some(&4), iter.next());
+        assert_eq!(Some(&5), iter.next());
+        assert_eq!(None, iter.next());
+
+        /// check all of elements are still alive
+        let mut iter = list.iter();
+        assert_eq!(Some(&0), iter.next());
+        assert_eq!(Some(&1), iter.next());
+        assert_eq!(Some(&2), iter.next());
+        assert_eq!(Some(&3), iter.next());
+        assert_eq!(Some(&4), iter.next());
+        assert_eq!(Some(&5), iter.next());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn iter_mut() {
+        let ls = [0, 1, 2, 3, 4, 5];
+        let mut list = List::new();
+
+        ls.iter().cloned().for_each(|e| list.push(e));
+
+        list.iter_mut().for_each(|elem| *elem *= 2);
+
+        assert!(ls
+            .iter()
+            .map(|e| e * 2)
+            .zip(list.iter().cloned())
+            .all(|(l, r)| l == r));
     }
 }
